0a7382d6c331ccb460ced813360ec1ff241310d8
Christian Kozalla <christian.kozalla@gmail.com>
2022-01-10 10:50:18 +0100

    Two CTA anchors point to app dir where react app is hosted

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: 'Puroviva',
 	description: 'Puroviva App Landing Page',
-	url: 'http://localhost:8080',
+	url: 'https://seaty.s3-website.nl-ams.scw.cloud/',
 	author: 'Christian Kozalla',
 	meta_data: {
 		twitter: '@CKozalla',
